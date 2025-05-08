43e62d4be80981f3be76fa9e73cb95c59697e31e
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-05-08 13:54:18 -0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,19 +8,6 @@ const api = axios.create({
   },
 });
 
-// Interceptor para añadir el token a todas las solicitudes
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    console.log("Solicitud enviada con token:", token); // Depuración
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
 // Interceptor para manejar respuestas y errores
 api.interceptors.response.use(
   (response) => response,
