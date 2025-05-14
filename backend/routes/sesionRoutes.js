3117a2ca7f9618b16e797a4a977c896a9716904f
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-05-14 18:29:58 -0500

    Update sesionRoutes.js

--- backend/routes/sesionRoutes.js
+++ backend/routes/sesionRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require("../middleware/auth");
+const { protect } = require("../middleware/authMiddleware");
 const {
   crearSesion,
   obtenerSesiones,
 } = require("../controllers/sesionController");
 
-router.post("/sesiones/crear", authMiddleware, crearSesion);
-router.get("/sesiones", authMiddleware, obtenerSesiones);
+router.post("/crear", protect, crearSesion);
+router.get("/", protect, obtenerSesiones);
 
 module.exports = router;
