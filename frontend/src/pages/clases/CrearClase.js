3d4b47277ea4d2f04b6231684a45bf263905c554
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-05-14 10:32:37 -0500

    Update CrearClase.js

--- frontend/src/pages/clases/CrearClase.js
+++ frontend/src/pages/clases/CrearClase.js
@@ -8,7 +8,7 @@ const CrearClase = () => {
   const [formData, setFormData] = useState({
     nombre: "",
     descripcion: "",
-    horario: [{ dia: "", hora: "" }], // Asegurado como array
+    horario: [{ dia: "", hora: "" }],
     capacidad: 1,
     entrenador: "",
     estado: "activa",
