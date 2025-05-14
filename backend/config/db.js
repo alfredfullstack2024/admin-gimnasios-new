6c7c9f660f06258c124bed31d87a44e9b62c5581
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-05-14 18:31:34 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("La variable de entorno MONGO_URI no está definida.");
+    }
     console.log("Intentando conectar a:", process.env.MONGO_URI);
-    const conn = await mongoose.connect(process.env.MONGO_URI); // Eliminamos opciones obsoletas
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error en la conexión a MongoDB: ${error.message}`);
