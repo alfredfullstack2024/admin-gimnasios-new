f4b97c09a4233b74fde09bda24b06697dff5ab4e
alfredfullstack2024 <alfredfullstack@gmail.com>
2025-05-16 13:53:24 -0500

    Update RegistroClase.js

--- backend/models/RegistroClase.js
+++ backend/models/RegistroClase.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 
-const registroClaseSchema = new mongoose.Schema({
-  numeroIdentificacion: { type: String, required: true },
-  entrenadorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Entrenador",
-    required: true,
+const registroClaseSchema = new mongoose.Schema(
+  {
+    numeroIdentificacion: { type: String, required: true },
+    nombre: { type: String, required: true }, // Nuevo campo para el nombre
+    apellido: { type: String, required: true }, // Nuevo campo para el apellido
+    entrenadorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Entrenador",
+      required: true,
+    },
+    nombreClase: { type: String, required: true },
+    dia: { type: String, required: true },
+    horarioInicio: { type: String, required: true },
+    horarioFin: { type: String, required: true },
   },
-  nombreClase: { type: String, required: true },
-  dia: { type: String, required: true },
-  horarioInicio: { type: String, required: true },
-  horarioFin: { type: String, required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("RegistroClase", registroClaseSchema);
